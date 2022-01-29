3a037a9ffbc88e5ad52999580b4b364c250ba3d7
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-01-29 10:17:49 +0500

    Change Projects table

--- database/migrations/2022_01_28_084435_create_projects_table.php
+++ database/migrations/2022_01_28_084435_create_projects_table.php
@@ -15,11 +15,17 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('project_name');
+            $table->text('general_info');
+            $table->text('general_file');
+            $table->foreignId('status_id');
+            $table->foreignId('developer_id');
+            $table->text('developer_info');
             $table->dateTime('start_date');
             $table->dateTime('dedline_date');
-            $table->text('general_info');
-            $table->foreignId('status_id');
-            $table->text('file');
+            $table->dateTime('finish_date');
+            $table->text('about_file');
+            $table->text('project_file');
             $table->timestamps();
         });
     }
