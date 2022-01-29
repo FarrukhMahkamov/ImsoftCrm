35377310cdd878dda923296f6f846d0bfef824cc
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-01-29 10:26:09 +0500

    Add fillable fields for Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'project_name',
+        'general_info',
+        'general_file',
+        'status_id',
+        'developer_id',
+        'developer_info',
+        'start_date',
+        'deadline_date',
+        'finish_date',
+        'about_file',
+        'project_file',
+    ];
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);    
+    }
 }
